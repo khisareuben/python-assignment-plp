9d9475db4888aea43c3fafea09f73925d6411239
Khisa Reuben <khisareuben@gmail.com>
2024-10-29 13:35:36 +0300

    my python week1 assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print("\n")  # Prints a newline
+
+print(f"Hello {name}, you are {age} and you live in {location}.")
