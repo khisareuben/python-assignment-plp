51ffcefdb21eda24293c99076ed66fab4d450dc8
khisareuben <156296475+khisareuben@users.noreply.github.com>
2024-10-31 12:41:37 +0300

    Add files via upload

---
+++ week-1-assignment/user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print()
+
+print(f"Hello {name}, you are {age} and you live in {location}.")
