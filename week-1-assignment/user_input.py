a845eb03856fbc78cf47c2dc80063709da461ddf
Khisa Reuben <khisareuben@gmail.com>
2024-10-31 13:27:46 +0300

    python asssignment

--- week-1-assignment/user_input.py
+++ week-1-assignment/user_input.py
@@ -1,7 +1,13 @@
+# Use the input() function to ask the user for their name and store it in a variable called "name".
 name = input("What is your name? ")
+
+# Use the input() function to ask the user for their age and store it in a variable called "age"
 age = input("How old are you? ")
+
+# Use the input() function to ask the user for their location and store it in a variable called "location"
 location = input("Where do you live? ")
 
+# Print out a personalized message using the user's name, age, and location.
 print()
 
 print(f"Hello {name}, you are {age} and you live in {location}.")
